2a1d03013186d60c9b8e18a0cb21d29f8f84e698
Stephanie Opala <rispahopala@gmail.com>
2023-03-27 16:52:09 +0300

    implement a queue data structure

---
+++ app.js
@@ -0,0 +1,40 @@
+// Implement a queue
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+  // inserting an element/item at the end
+  enqueue(item) {
+    this.items.push(item);
+  }
+
+  // removing an element/item at the front of the queue
+  dequeue() {
+    if(this.items.length === 0) {
+      return;
+    } else {
+      this.items.shift();
+    }
+  }
+
+  // check if queue is empty
+  isEmpty() {
+    this.items.length === 0;
+  }
+
+  //get the element at the front of the queue
+  peek() {
+    if(this.items.length === 0) {
+      return "Queue is empty";
+    } else {
+      return this.items[0];
+    }
+  }
+}
+
+const newQueue = new Queue();
+newQueue.enqueue(14);
+newQueue.enqueue(90);
+newQueue.enqueue(36);
+newQueue.dequeue();
+console.log(newQueue.peek());
